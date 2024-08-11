4bf0e5f5efb2a2014a87c5d6e1e29915bae981e1
hanasaki <hanasakayui2022@gmail.com>
2024-08-11 17:20:20 +0800

    📦️:使用localstore插件管理资源文件，移除pydantic版本限制

--- nonebot_plugin_ba_tools/config.py
+++ nonebot_plugin_ba_tools/config.py
@@ -1,12 +1,17 @@
 from typing import List
 from pathlib import Path
+from nonebot import require
 from pydantic import BaseModel
+
+require("nonebot_plugin_localstore")
+
+from nonebot_plugin_localstore import get_plugin_cache_dir
 
 
 class Config(BaseModel):
     """Plugin Config Here"""
 
     # 资源文件路径
-    assert_path: Path = Path(__file__).resolve().parents[1] / "asserts"
+    assert_path: Path = get_plugin_cache_dir() / "ba-tools/asserts"
     # 订阅推送消息的群聊列表
     send_daily_info_group_list: List[str] = ["414151515"]
