96520e4507fd13540d19a14836ecf8ebdc2c37dc
hanasaki <hanasakayui2022@gmail.com>
2024-08-11 20:25:12 +0800

    🐛:修复缓存文件夹位置错误问题

--- nonebot_plugin_ba_tools/config.py
+++ nonebot_plugin_ba_tools/config.py
@@ -12,6 +12,6 @@ class Config(BaseModel):
     """Plugin Config Here"""
 
     # 资源文件路径
-    assert_path: Path = get_plugin_cache_dir() / "ba-tools/asserts"
+    assert_path: Path = get_plugin_cache_dir() / "asserts"
     # 订阅推送消息的群聊列表
     send_daily_info_group_list: List[str] = ["414151515"]
