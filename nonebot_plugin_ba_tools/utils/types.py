bcd29ef49e1dec2f0dd08dcc655802c5d8d0588b
hanasaki <hanasakayui2022@gmail.com>
2024-08-13 21:21:14 +0800

    ✨:完善Student类解析类型

--- nonebot_plugin_ba_tools/utils/types.py
+++ nonebot_plugin_ba_tools/utils/types.py
@@ -1,17 +1,16 @@
 import json
 from typing import Any, Dict, List
 
-from httpx import AsyncClient, HTTPStatusError
-from nonebot import logger
+from httpx import AsyncClient
 
 
 class Student:
     def __init__(self, data: Dict[str, Any]):
-        self.id = data.get("Id")
-        self.is_released = data.get("IsReleased")
-        self.name = data.get("Name")
-        self.personal_name = data.get("PersonalName")
-        self.birthday = data.get("Birthday")
+        self.id = str(data.get("Id"))
+        self.is_released = [d for d in data.get("IsReleased", [])]
+        self.name = str(data.get("Name"))
+        self.personal_name = str(data.get("PersonalName"))
+        self.birthday = str(data.get("Birthday"))
         self.skills = [Skill(skill) for skill in data.get("Skills", [])]
         self.weapon = Weapon(data.get("Weapon", {}))
         self.gear = Gear(data.get("Gear", {}))
@@ -52,7 +51,7 @@ class DataLoadError(Exception):
 
 
 class StudentParser:
-    """对students.json的解析类，若file_path属性不为空，则从解析本地文件，若url不为空，则解析网络文件，否则抛出异常"""
+    """对students.json的解析类，若file_path属性不为空，则从本地解析文件，若file_path为空且url不为空，则解析网络文件，否则抛出异常"""
 
     def __init__(self, file_path: str | None = None, url: str | None = None):
         self.file_path = file_path
@@ -65,22 +64,14 @@ class StudentParser:
             List[Student]: Student类的列表
         """
         if self.file_path:
-            async with open(self.file_path, "r", encoding="utf-8") as file:
+            with open(self.file_path, "r", encoding="utf-8") as file:
                 data = json.load(file)
                 return [Student(student) for student in data]
         elif self.url:
             async with AsyncClient() as client:
-                try:
-                    response = await client.get(self.url)
-                    response.raise_for_status()
-                    data = response.json()
-                    return [Student(student) for student in data]
-                except HTTPStatusError as e:
-                    logger.error(e)
-                except Exception as e:
-                    logger.error(e)
+                response = await client.get(self.url)
+                response.raise_for_status()
+                data = response.json()
+                return [Student(student) for student in data]
         else:
-            try:
-                raise DataLoadError("数据加载错误,请检查file_path或url是否正确")
-            except DataLoadError as e:
-                logger.error(e)
+            raise DataLoadError("数据加载错误,请检查file_path或url是否填写")
