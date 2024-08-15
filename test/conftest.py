05d3fb5fa975483e309b5bc0743c549a3501e380
hanasaki <hanasakayui2022@gmail.com>
2024-08-16 00:33:28 +0800

    ✅:添加单元测试

---
+++ test/conftest.py
@@ -0,0 +1,16 @@
+import nonebot
+import pytest
+from nonebot.adapters.onebot.v11 import Adapter
+from nonebug import NONEBOT_INIT_KWARGS
+
+
+@pytest.fixture(scope="session", autouse=True)
+def load_bot():
+    driver = nonebot.get_driver()
+    driver.register_adapter(Adapter)
+
+    nonebot.load_from_toml("pyproject.toml")
+
+
+def pytest_configure(config: pytest.Config):
+    config.stash[NONEBOT_INIT_KWARGS] = {"command_start": {"/"}, "log_level": "DEBUG"}
