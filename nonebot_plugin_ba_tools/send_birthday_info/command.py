1c48fcdf690ae4f2e51a08b7f7585c2eadae49ef
hanasaki <hanasakayui2022@gmail.com>
2024-08-18 00:37:37 +0800

    🎨 优化指令

--- nonebot_plugin_ba_tools/send_birthday_info/command.py
+++ nonebot_plugin_ba_tools/send_birthday_info/command.py
@@ -1,9 +1,10 @@
 import json
+from pathlib import Path
 
 from arclet.alconna import Alconna, Subcommand
 from nonebot import logger
 from nonebot.adapters.onebot.v11 import Bot, GroupMessageEvent
-from nonebot_plugin_alconna import Arparma, on_alconna
+from nonebot_plugin_alconna import Args, Arparma, Match, on_alconna
 
 from ..config import DRIVER, plugin_config
 from ..utils.constants import BIRTHDAY_INFO_GROUP_LIST_FILE
@@ -18,7 +19,7 @@ GROUP_LIST: list[int] = []
 
 
 def save_group_list():
-    full_path = plugin_config.setting_path / BIRTHDAY_INFO_GROUP_LIST_FILE
+    full_path: Path = plugin_config.setting_path / BIRTHDAY_INFO_GROUP_LIST_FILE
     if not plugin_config.setting_path.exists():
         plugin_config.setting_path.mkdir(parents=True, exist_ok=True)
     with open(full_path, "w", encoding="utf-8") as f:
@@ -29,7 +30,7 @@ def save_group_list():
 async def _():
     logger.debug("读取生日信息推送群列表")
     global GROUP_LIST
-    full_path = plugin_config.setting_path / BIRTHDAY_INFO_GROUP_LIST_FILE
+    full_path: Path = plugin_config.setting_path / BIRTHDAY_INFO_GROUP_LIST_FILE
     if not full_path.exists():
         if not plugin_config.setting_path.exists():
             plugin_config.setting_path.mkdir(parents=True, exist_ok=True)
@@ -41,13 +42,13 @@ async def _():
         logger.debug(f"group list is: {GROUP_LIST}")
 
 
-birthday_info = Alconna("ba_birthday_info", Subcommand("on"), Subcommand("off"))
+# TODO: 添加命令别名
+birthday_info = Alconna("ba学生生日订阅", Args["status", str])
 birthday_info_switch = on_alconna(birthday_info, use_cmd_start=True)
 
 
-# TODO: 添加命令别名
-@birthday_info_switch.handle()
-async def _(bot: Bot, event: GroupMessageEvent, result: Arparma):
+@birthday_info_switch.assign("status")
+async def _(bot: Bot, event: GroupMessageEvent, status: Match[str]):
     global GROUP_LIST
     # 群主、管理员、SUPERUSER可以使用此命令
     user_info = await get_group_user_info(bot, event.user_id, event.group_id)
@@ -56,21 +57,22 @@ async def _(bot: Bot, event: GroupMessageEvent, result: Arparma):
         or is_group_admin(user_info)
         or is_superuser(event.user_id)
     ):
-        if result.find("on"):
-            if event.group_id not in GROUP_LIST:
-                GROUP_LIST.append(event.group_id)
-                save_group_list()
-                await birthday_info_switch.finish("已开启生日信息推送")
+        if status.available:
+            if status.result == "开启":
+                if event.group_id not in GROUP_LIST:
+                    GROUP_LIST.append(event.group_id)
+                    save_group_list()
+                    await birthday_info_switch.finish("已开启生日信息推送")
+                else:
+                    await birthday_info_switch.finish("已开启生日信息推送")
+            elif status.result == "关闭":
+                if event.group_id in GROUP_LIST:
+                    GROUP_LIST.remove(event.group_id)
+                    save_group_list()
+                    await birthday_info_switch.finish("已关闭生日信息推送")
+                else:
+                    await birthday_info_switch.finish("已关闭生日信息推送")
             else:
-                await birthday_info_switch.finish("已开启生日信息推送")
-        elif result.find("off"):
-            if event.group_id in GROUP_LIST:
-                GROUP_LIST.remove(event.group_id)
-                save_group_list()
-                await birthday_info_switch.finish("已关闭生日信息推送")
-            else:
-                await birthday_info_switch.finish("已关闭生日信息推送")
-        else:
-            await birthday_info_switch.finish("无效的指令")
+                await birthday_info_switch.finish("无效的指令")
     else:
         await birthday_info_switch.finish("权限不足")
