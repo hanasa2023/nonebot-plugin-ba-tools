feaa1a8bd836ccacc8531e3eb0b8c974c4de2b95
hanasaki <hanasakayui2022@gmail.com>
2024-08-18 00:38:23 +0800

    ✨ 新增千里眼/活动工具类

--- nonebot_plugin_ba_tools/utils/constants.py
+++ nonebot_plugin_ba_tools/utils/constants.py
@@ -4,3 +4,8 @@ BIRTHDAY_INFO_GROUP_LIST_FILE = "birthday_info_group_list.json"
 
 DATA_STUDENTS_JSON_FILE_PATH = "data/zh/students.json"
 DATA_STUDENT_JSON_FOLDER_PATH = "data/zh/student"
+
+WIKI_BASE_URL = "https://www.gamekee.com"
+BA_WIKI_URL = WIKI_BASE_URL + "/ba"
+
+ACTIVITYT_HTML_PATH = "activity.html"
