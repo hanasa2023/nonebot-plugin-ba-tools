097fa3e7fd84e46cd62c2bc01d0eafc127a1522b
hanasaki <hanasakayui2022@gmail.com>
2024-11-06 00:14:18 +0800

    ♻ 重构千里眼代码

--- nonebot_plugin_ba_tools/get_ba_clairvoyance/__init__.py
+++ nonebot_plugin_ba_tools/get_ba_clairvoyance/__init__.py
@@ -1,14 +1,10 @@
 from typing import Any
 
-from bs4 import BeautifulSoup
 from nonebot import require
 from nonebot.adapters.onebot.v11 import Bot
 
 from ..config import plugin_config
-from ..utils.wiki import get_data_from_html, get_wiki_url_from_title
-from .utils.get_clairvoyance_img import (
-    create_clairvoyance_img,
-)
+from .utils.get_clairvoyance_img import get_img
 
 require("nonebot_plugin_alconna")
 from nonebot_plugin_alconna import (  # noqa: E402
@@ -30,29 +26,13 @@ get_ba_clairvoyance: type[AlconnaMatcher] = on_alconna(clairvoyance, use_cmd_sta
 async def _(bot: Bot, server: Match[str]) -> None:
     if server.available:
         pre_msg: dict[str, int] = {"message_id": -1}
-        if server.result == "国际服":
-            url = await get_wiki_url_from_title("国际服千里眼")
-            if url:
-                if plugin_config.loading_switch:
-                    pre_msg = await get_ba_clairvoyance.send("拼命加载图片中……")
-                msg = await create_clairvoyance_img(url)
-                await get_ba_clairvoyance.send(msg)
-                if plugin_config.loading_switch:
-                    await bot.delete_msg(message_id=pre_msg["message_id"])
-                await get_ba_clairvoyance.finish()
-        elif server.result == "国服":
-            url = await get_wiki_url_from_title("国服卡池千里眼")
-            if url:
-                if plugin_config.loading_switch:
-                    pre_msg = await get_ba_clairvoyance.send("拼命加载图片中……")
-                text = await get_data_from_html(url)
-                soup = BeautifulSoup(text, "html.parser")
-                imgs = soup.select(".preview-image")
-                src = imgs[0].get("src")
-                msg = UniMessage([Image(url=f"https:{src}")])
-                await get_ba_clairvoyance.send(msg)
-                if plugin_config.loading_switch:
-                    await bot.delete_msg(message_id=pre_msg["message_id"])
-                await get_ba_clairvoyance.finish()
+        msg: UniMessage[Image] | None = await get_img(server.result)
+        if msg:
+            if plugin_config.loading_switch:
+                pre_msg = await get_ba_clairvoyance.send("拼命加载图片中……")
+            await get_ba_clairvoyance.send(msg)
+            if plugin_config.loading_switch:
+                await bot.delete_msg(message_id=pre_msg["message_id"])
+            await get_ba_clairvoyance.finish()
         else:
             await get_ba_clairvoyance.finish("不支持的服务器哦～")
--- nonebot_plugin_ba_tools/get_ba_clairvoyance/utils/get_clairvoyance_img.py
+++ nonebot_plugin_ba_tools/get_ba_clairvoyance/utils/get_clairvoyance_img.py
@@ -1,50 +1,50 @@
-from multiprocessing.context import ProcessError
 from pathlib import Path
 
 import httpx
-from bs4 import BeautifulSoup
-from nonebot import require
-
-from nonebot_plugin_ba_tools.utils.constants import ASSERTS_CLAIRVOYANCE_IMG_PATH
+from nonebot import logger, require
 
 from ...config import plugin_config
-from ...utils.common import get_data_from_html
+from ...utils.constants import ASSERTS_CLAIRVOYANCE_IMG_PATH
 
 require("nonebot_plugin_alconna")
 from nonebot_plugin_alconna import Image, UniMessage  # noqa: E402
 
 
-async def create_clairvoyance_img(url: str) -> UniMessage[Image]:
+async def get_img(server: str) -> UniMessage[Image] | None:
     """创建千里眼图片，若本地不存在，则从wiki缓存到本地
 
     Args:
-        url (str): 千里眼的 wiki url
+        server (str): 服务器名(国际服/国服)
 
     Returns:
-        UniMessage[Image]: image消息
+        UniMessage[Image] | None: 若获取到千里眼信息，则返回image消息否则返回None
     """
-    text = await get_data_from_html(url)
-    soup = BeautifulSoup(text, "html.parser")
-    imgs = soup.select(".div-img > img")
-    src = imgs[3].get("src") if len(imgs) >= 3 else imgs[0].get("src")
-    if isinstance(src, str):
-        img_path: Path = (
-            plugin_config.assert_path
-            / ASSERTS_CLAIRVOYANCE_IMG_PATH
-            / f"{src.split('/')[-1]}.png"
-        )
-        if not img_path.exists():
-            # 若父文件夹不存在则创建文件夹
-            folder = img_path.parent
-            if not folder.exists():
-                folder.mkdir(parents=True, exist_ok=True)
-            # 获取图片
-            async with httpx.AsyncClient() as ctx:
-                response: httpx.Response = await ctx.get(f"https:{src}")
-                if response.status_code == 200:
-                    with open(file=img_path, mode="wb") as f:
-                        f.write(response.content)
-        msg: UniMessage[Image] = UniMessage(Image(path=img_path))
-        return msg
-    else:
-        raise ProcessError("获取图片失败")
+    url: str = f"https://tutorial.arona.diyigemt.com/api/v2/image?name={server}未来视"
+    async with httpx.AsyncClient() as ctx:
+        response: httpx.Response = await ctx.get(url)
+        if response.status_code == 200:
+            res_json = response.json()
+            hash = res_json["data"][0]["hash"]
+            if hash:
+                img_path: Path = (
+                    plugin_config.assert_path
+                    / ASSERTS_CLAIRVOYANCE_IMG_PATH
+                    / f"{hash}.png"
+                )
+                if not img_path.exists():
+                    logger.debug(f"千里眼图片不存在，正在从网络下载至{img_path}……")
+                    # 若父文件夹不存在则创建文件夹
+                    folder: Path = img_path.parent
+                    if not folder.exists():
+                        folder.mkdir(parents=True, exist_ok=True)
+                    # 获取图片
+                    async with httpx.AsyncClient() as ctx:
+                        res: httpx.Response = await ctx.get(
+                            f"https://arona.cdn.diyigemt.com/image/some/{server}未来视.png?{hash}"
+                        )
+                        if res.status_code == 200:
+                            with open(file=img_path, mode="wb") as f:
+                                f.write(res.content)
+
+                logger.debug(f"从{img_path}加载图片……")
+                return UniMessage.image(path=img_path)
