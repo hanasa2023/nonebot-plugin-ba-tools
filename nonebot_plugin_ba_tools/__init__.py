6fada08437b17098cd0620a37824fac5c5914c33
hanasaki <hanasakayui2022@gmail.com>
2024-11-10 08:23:58 +0800

    🔖 修改版号

--- nonebot_plugin_ba_tools/__init__.py
+++ nonebot_plugin_ba_tools/__init__.py
@@ -27,7 +27,7 @@ from .send_birthday_info import (
     send_birthday_info as send_birthday_info,
 )
 
-__version__ = "0.3.4"
+__version__ = "0.3.3"
 __plugin_meta__ = PluginMetadata(
     name="ba-tools",
     description="BlueArchive的工具箱",
