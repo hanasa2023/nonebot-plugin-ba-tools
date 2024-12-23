1dfb5571294b62e8aaf7b06d264829387614471d
hanasaki <hanasakayui2022@gmail.com>
2024-12-23 18:22:07 +0800

    ✨ 新增可用攻略获取指令

--- nonebot_plugin_ba_tools/get_ba_walkthrough/__init__.py
+++ nonebot_plugin_ba_tools/get_ba_walkthrough/__init__.py
@@ -1,8 +1,11 @@
 from __future__ import annotations
+import httpx
 
 from typing import Any
 
 from nonebot import require
+
+from ..utils.constants import ARONA_CDN_URL
 
 from ..config import plugin_config
 from ..utils.get_img_from_name import get_img
@@ -19,9 +22,13 @@ from nonebot_plugin_alconna import (  # noqa: E402
 )
 from nonebot_plugin_alconna.uniseg import Receipt  # noqa: E402
 
-# TODO: 添加命令别名
 walkthrough: Alconna[Any] = Alconna("ba攻略", Args["option", str])
 get_walkthrough: type[AlconnaMatcher] = on_alconna(walkthrough, use_cmd_start=True)
+
+walkthrough_list: Alconna[Any] = Alconna("ba可用攻略")
+get_walkthrough_list: type[AlconnaMatcher] = on_alconna(
+    walkthrough_list, use_cmd_start=True
+)
 
 
 @get_walkthrough.assign("option")
@@ -29,7 +36,9 @@ async def _(option: Match[str]) -> None:
     if option.available:
         pre_msg: Receipt | None = None
         type: str = "关卡攻略" if option.result.startswith("关卡") else option.result
-        middle_route: str = "chapter-map" if option.result.startswith("关卡") else "strategy"
+        middle_route: str = (
+            "chapter-map" if option.result.startswith("关卡") else "strategy"
+        )
         msg: UniMessage[Image] | None = await get_img(
             option.result.replace("关卡", ""), type, middle_route
         )
@@ -42,3 +51,19 @@ async def _(option: Match[str]) -> None:
             await get_walkthrough.finish()
         else:
             await get_walkthrough.finish(f"未找到对应{option.result}的攻略哦～")
+
+
+@get_walkthrough_list.handle()
+async def _() -> None:
+    async with httpx.AsyncClient() as ctx:
+        response: httpx.Response = await ctx.get(
+            f"{ARONA_CDN_URL}/data/available-list.json"
+        )
+        if response.status_code == 200:
+            walkthroughs: list[str] = response.json()["walkthrough"]
+            msg = "可用的攻略有：\n"
+            for name in walkthroughs:
+                msg += f"- {name}\n"
+
+            await get_walkthrough_list.finish(msg)
+    await get_walkthrough_list.finish("获取出错了")
