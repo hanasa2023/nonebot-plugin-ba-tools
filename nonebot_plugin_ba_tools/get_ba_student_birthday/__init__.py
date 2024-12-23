db1a76c9982812662ab27f76b77550e4ab6eb230
hanasaki <hanasakayui2022@gmail.com>
2024-12-23 18:27:41 +0800

    ✨ 新增获取学生生日分布图的指令

--- nonebot_plugin_ba_tools/get_ba_student_birthday/__init__.py
+++ nonebot_plugin_ba_tools/get_ba_student_birthday/__init__.py
@@ -4,11 +4,12 @@ import re
 from datetime import datetime
 from typing import Any
 
+import httpx
 from nonebot import require
 
 from ..config import plugin_config
 from ..utils.common import get_students_by_birth_month
-from ..utils.constants import DATA_STUDENTS_BIRTHDAY_IMG_PATH
+from ..utils.constants import ARONA_API_URL, DATA_STUDENTS_BIRTHDAY_IMG_PATH
 from ..utils.types import Student
 from .utils.draw_img import (
     init_birthday_img,
@@ -21,6 +22,7 @@ from nonebot_plugin_alconna import (  # noqa: E402
     Args,
     Image,
     Match,
+    Text,
     UniMessage,
     on_alconna,
 )
@@ -30,6 +32,9 @@ birthday_list: Alconna[Any] = Alconna("ba学生生日表", Args["month", str])
 get_student_birthday_list: type[AlconnaMatcher] = on_alconna(
     birthday_list, use_cmd_start=True
 )
+
+birthday_map: Alconna[Any] = Alconna("ba学生生日分布")
+get_birthday_map: type[AlconnaMatcher] = on_alconna(birthday_map, use_cmd_start=True)
 
 
 @get_student_birthday_list.assign("month")
@@ -58,3 +63,16 @@ async def _(month: Match[str]) -> None:
                 await get_student_birthday_list.finish()
             else:
                 await get_student_birthday_list.finish("是无效的月份呢")
+
+
+@get_birthday_map.handle()
+async def _() -> None:
+    msg: UniMessage[Image | Text] = UniMessage.text("获取失败了😢")
+    async with httpx.AsyncClient() as ctx:
+        response: httpx.Response = await ctx.get(
+            f"{ARONA_API_URL}/api/student/birthday/distribution"
+        )
+        if response.status_code == 200:
+            msg = UniMessage.image(url=response.json()["data"]["imgUrl"])
+
+    await get_birthday_map.finish(msg)
