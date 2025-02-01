81cd84f1ef7d499d5cb325ef6826409f492e00e7
hanasaki <hanasakayui2022@gmail.com>
2025-02-01 23:47:18 +0800

    ⚡️ 优化WebUI

--- nonebot_plugin_ba_tools/__init__.py
+++ nonebot_plugin_ba_tools/__init__.py
@@ -41,7 +41,7 @@ from .send_birthday_info import (
 )
 from .webui import app as app
 
-__version__ = "0.5.0-beta"
+__version__ = "0.5.0-alpha"
 __plugin_meta__ = PluginMetadata(
     name="ba-tools",
     description="BlueArchive的工具箱",
