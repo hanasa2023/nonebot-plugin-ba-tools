85a743adc6e8c8e686a56e0edfe3341e0ec192a1
hanasaki <hanasakayui2022@gmail.com>
2025-02-04 23:02:08 +0800

    🐛 Fix app cannot load bug

--- nonebot_plugin_ba_tools/webui/driver.py
+++ nonebot_plugin_ba_tools/webui/driver.py
@@ -1,22 +1,22 @@
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from nonebot import get_driver
+from nonebot.drivers import ReverseDriver
 from nonebot.drivers.fastapi import Driver as FastAPIDriver
 
-from ..config import ConfigManager
 
-driver = get_driver()
+driver: FastAPIDriver = get_driver()  # type: ignore
 
-if ConfigManager.get().webui.enable:
-    if not isinstance(driver, FastAPIDriver):
-        raise NotImplementedError("This feature only support FastAPI driver.")
+if not isinstance(driver, ReverseDriver) or not isinstance(driver, FastAPI):
+    raise NotImplementedError("This feature only support FastAPI driver.")
 
-    app = driver.server_app
+app = driver.server_app
 
-    app.separate_input_output_schemas = False
-    app.add_middleware(
-        CORSMiddleware,
-        allow_origins=["*"],
-        allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"],
-    )
+app.separate_input_output_schemas = False
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
