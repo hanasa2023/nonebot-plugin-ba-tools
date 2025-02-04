2826c886fa22e96d9c7728525c870ddfe9e845bc
hanasaki <hanasakayui2022@gmail.com>
2025-02-04 08:01:58 +0800

    🔥 移除onebot adapter的使用

--- nonebot_plugin_ba_tools/__init__.py
+++ nonebot_plugin_ba_tools/__init__.py
@@ -41,7 +41,7 @@ from .send_birthday_info import (
 )
 from .webui import app as app
 
-__version__ = "0.5.5"
+__version__ = "0.5.6"
 __plugin_meta__ = PluginMetadata(
     name="ba-tools",
     description="BlueArchive的工具箱",
--- nonebot_plugin_ba_tools/utils/user_info.py
+++ nonebot_plugin_ba_tools/utils/user_info.py
@@ -1,21 +1,6 @@
 from __future__ import annotations
 
-from typing import Any
-
-from nonebot import logger
-from nonebot.adapters.onebot.v11 import Bot as OneV11Bot
-
 from ..config import SUPERUSERS
-
-
-# TODO: 实现多平台支持
-async def get_group_user_info(bot: OneV11Bot, user_id: int, group_id: int, no_cache: bool = False) -> dict[str, Any]:
-    try:
-        user_info = await bot.get_group_member_info(user_id=user_id, group_id=group_id, no_cache=no_cache)
-        return user_info
-    except Exception as e:
-        logger.exception(e)
-    return {"card": "未知用户", "nickname": str(user_id), "role": "none"}
 
 
 def is_group_owner(user_info: dict[str, str]) -> bool:
